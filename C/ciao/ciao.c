79759bfde219a684c7e7c0b8ab4250704da5ab67
Noble <francesco03.ricciardi@gmail.com>
2023-12-06 16:29:42 +0100

    Deleted C/ciao/ciao.c

--- C/ciao/ciao.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("hello");
-    printf("No ma guarda che puoi mettere anche questo");
-    return 0;
-}
